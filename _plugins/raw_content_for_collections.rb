009f335bc6de4d15435722383498c866e4860f39
tony-xlh <tony@dynamsoft.com>
2026-01-19 11:19:07 +0800

    fix(raw content plugin): remove yaml head

--- _plugins/raw_content_for_collections.rb
+++ _plugins/raw_content_for_collections.rb
@@ -7,25 +7,27 @@ module Jekyll
     def generate(site)
       site.collections.each do |label, collection|
         next unless label == 'articles'
-
         collection.docs.each do |doc|
           source_path = File.join(site.source, doc.relative_path)
+          
           if File.exist?(source_path)
-            raw_file_content = File.read(source_path)
-
-            doc.data['raw_content'] = raw_file_content
+            raw_file_content = File.read(source_path, encoding: 'UTF-8')
+            content_without_front_matter = remove_front_matter(raw_file_content)
+            doc.data['raw_content'] = content_without_front_matter
           end
         end
       end
+      
     end
 
     private
 
     def remove_front_matter(content)
-      if content =~ /\A(---\s*\n.*?\n---\s*\n)/m
-        content.sub(/\A(---\s*\n.*?\n---\s*\n)/m, '')
+      if content =~ /\A---\s*\n.*?\n---\s*\n+/m
+        result = content.sub(/\A---\s*\n.*?\n---\s*\n+/m, '')
+        return result
       else
-        content
+        return content
       end
     end
   end
